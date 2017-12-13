5a82ed9fe9d575cf9034b3e3ee70771189815f83
alexrewrew <alexrew2012@gmail.com>
2017-12-13 13:33:44 +0200

    Update 13.12

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -28,7 +28,6 @@ $(document).ready(function () {
     });
 
 
-
     $(".slider-two").slick({
         // slide: ".slide",
         dots: true,
@@ -111,6 +110,21 @@ $(document).ready(function () {
 
 
     });
+
+    // ----- RANGE SLIDER -----
+    $( function() {
+        $( "#slider-range" ).slider({
+            range: true,
+            min: 0,
+            max: 500,
+            values: [ 75, 300 ],
+            slide: function( event, ui ) {
+                $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+            }
+        });
+        $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+            " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+    } );
 
     // ----- SELECT -----
 
